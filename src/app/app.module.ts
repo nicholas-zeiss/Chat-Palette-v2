eb366bfee874a3803c858dd6d56b8021a6093c53
nicholas-zeiss <nicholas.zeiss@gmail.com>
2018-02-07 02:31:19 -0500

    created base level components, added routing to display them

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,48 @@
+
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { ChatComponent } from './chat/chat.component';
+import { SignupComponent } from './signup/signup.component';
+import { LoginComponent } from './login/login.component';
+
+
+const routes: Routes = [
+	{
+		path: 'chat',
+		component:	ChatComponent
+	},
+	{
+		path: 'login',
+		component: LoginComponent
+	},
+	{
+		path: 'signup',
+		component: SignupComponent
+	},
+	{
+		path: '**',
+		redirectTo: '/login'
+	}
+];
 
 
 @NgModule({
 	declarations: [
-		AppComponent
+		AppComponent,
+		ChatComponent,
+		SignupComponent,
+		LoginComponent
 	],
 	imports: [
-		BrowserModule
+		BrowserModule,
+		RouterModule.forRoot(routes)
 	],
 	providers: [],
-	bootstrap: [AppComponent]
+	bootstrap: [ AppComponent ]
 })
 export class AppModule { }
+
