694504912772bf6e8fbf214361f31e13cc03ad60
nicholas-zeiss <nicholas.zeiss@gmail.com>
2018-02-08 14:33:02 -0500

    created chat module files

---
+++ src/app/chat/chat.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ChatRoutingModule } from './chat-routing.module';
+import { ChatComponent } from './chat/chat.component';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    ChatRoutingModule
+  ],
+  declarations: [ChatComponent]
+})
+export class ChatModule { }
